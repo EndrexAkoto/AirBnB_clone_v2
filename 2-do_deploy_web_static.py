5eb5bf4d315b71501b82129dacf0536bbcf91525
koechMR <briankipkoech328@gmail.com>
2023-10-05 16:07:04 +0300

    new fix to the deployment static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,6 +8,7 @@ from fabric.api import run
 
 env.hosts = ["34.201.174.77", "100.26.174.137"]
 
+
 def do_deploy(archive_path):
     """Distribute an archive to a web server"""
     if not os.path.isfile(archive_path):
@@ -22,17 +23,21 @@ def do_deploy(archive_path):
         return False
     if run("mkdir -p /data/web_static/releases/{}/".format(name)).failed:
         return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name)).failed:
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+           .format(file, name)).failed:
         return False
     if run("rm /tmp/{}".format(file)).failed:
         return False
-    if run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name)).failed:
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".format(name)).failed:
+    if run("rm -rf /data/web_static/releases/{}/web_static"
+           .format(name)).failed:
         return False
     if run("rm -rf /data/web_static/current").failed:
         return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)).failed:
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+           .format(name)).failed:
         return False
 
     return True
