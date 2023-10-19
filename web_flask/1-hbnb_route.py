a3709576da44917ce0eab95870c5e7761d1916b9
koechMR <briankipkoech328@gmail.com>
2023-10-19 15:02:14 +0300

    routes

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,7 +2,6 @@
 """
 This is a basic flask application
 """
-
 from flask import Flask
 
 app = Flask(__name__)
@@ -11,6 +10,10 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
